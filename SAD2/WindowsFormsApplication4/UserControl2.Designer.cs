8401a8364788bfeca0100925d1a6ed9b301370b2
czshadow09 <reromarj@gmail.com>
2018-01-13 09:12:35 +0800

    Update UserControl2.Designer.cs

--- SAD2/WindowsFormsApplication4/UserControl2.Designer.cs
+++ SAD2/WindowsFormsApplication4/UserControl2.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication4
+namespace WindowsFormsApplication4
 {
     partial class UserControl2
     {
@@ -34,7 +34,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.search = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
             this.lastname = new System.Windows.Forms.TextBox();
             this.firstname = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -56,10 +56,10 @@
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(356, 607);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(267, 493);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(63, 57);
+            this.button3.Size = new System.Drawing.Size(47, 46);
             this.button3.TabIndex = 58;
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = true;
@@ -67,10 +67,10 @@
             // search
             // 
             this.search.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.search.Location = new System.Drawing.Point(28, 618);
-            this.search.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.search.Location = new System.Drawing.Point(21, 502);
+            this.search.Margin = new System.Windows.Forms.Padding(2);
             this.search.Name = "search";
-            this.search.Size = new System.Drawing.Size(321, 36);
+            this.search.Size = new System.Drawing.Size(242, 30);
             this.search.TabIndex = 57;
             // 
             // button2
@@ -81,42 +81,42 @@
             this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(199, 498);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(149, 405);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(179, 69);
+            this.button2.Size = new System.Drawing.Size(134, 56);
             this.button2.TabIndex = 56;
             this.button2.Text = "Update";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // Add
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(32, 502);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 62);
-            this.button1.TabIndex = 55;
-            this.button1.Text = " Add";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Add.FlatAppearance.BorderSize = 0;
+            this.Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Add.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.ForeColor = System.Drawing.Color.Black;
+            this.Add.Image = ((System.Drawing.Image)(resources.GetObject("Add.Image")));
+            this.Add.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Add.Location = new System.Drawing.Point(24, 408);
+            this.Add.Margin = new System.Windows.Forms.Padding(2);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(107, 50);
+            this.Add.TabIndex = 55;
+            this.Add.Text = " Add";
+            this.Add.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
             // lastname
             // 
             this.lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lastname.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.lastname.Location = new System.Drawing.Point(28, 123);
-            this.lastname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lastname.Location = new System.Drawing.Point(21, 100);
+            this.lastname.Margin = new System.Windows.Forms.Padding(2);
             this.lastname.Name = "lastname";
-            this.lastname.Size = new System.Drawing.Size(375, 34);
+            this.lastname.Size = new System.Drawing.Size(282, 28);
             this.lastname.TabIndex = 54;
             this.lastname.Text = "Lastname";
             this.lastname.Click += new System.EventHandler(this.textBox2_Click);
@@ -126,10 +126,10 @@
             // 
             this.firstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.firstname.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.firstname.Location = new System.Drawing.Point(28, 75);
-            this.firstname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.firstname.Location = new System.Drawing.Point(21, 61);
+            this.firstname.Margin = new System.Windows.Forms.Padding(2);
             this.firstname.Name = "firstname";
-            this.firstname.Size = new System.Drawing.Size(375, 34);
+            this.firstname.Size = new System.Drawing.Size(282, 28);
             this.firstname.TabIndex = 53;
             this.firstname.Text = "Firstname";
             this.firstname.Click += new System.EventHandler(this.textBox1_Click);
@@ -140,9 +140,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(21, 20);
+            this.label2.Location = new System.Drawing.Point(16, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(285, 33);
+            this.label2.Size = new System.Drawing.Size(221, 26);
             this.label2.TabIndex = 52;
             this.label2.Text = "Create Or Update User";
             // 
@@ -151,10 +152,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(425, 2);
-            this.label1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.label1.Location = new System.Drawing.Point(319, 2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(595, 46);
+            this.label1.Size = new System.Drawing.Size(446, 37);
             this.label1.TabIndex = 51;
             this.label1.Text = "User List";
             // 
@@ -174,21 +175,21 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(425, 46);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(319, 37);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(595, 640);
+            this.dataGridView1.Size = new System.Drawing.Size(446, 520);
             this.dataGridView1.TabIndex = 50;
             // 
             // id
             // 
             this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.id.Location = new System.Drawing.Point(347, 20);
-            this.id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.id.Location = new System.Drawing.Point(260, 16);
+            this.id.Margin = new System.Windows.Forms.Padding(2);
             this.id.Name = "id";
-            this.id.Size = new System.Drawing.Size(56, 36);
+            this.id.Size = new System.Drawing.Size(43, 30);
             this.id.TabIndex = 63;
             this.id.Visible = false;
             // 
@@ -196,10 +197,10 @@
             // 
             this.contact.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.contact.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.contact.Location = new System.Drawing.Point(28, 225);
-            this.contact.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.contact.Location = new System.Drawing.Point(21, 183);
+            this.contact.Margin = new System.Windows.Forms.Padding(2);
             this.contact.Name = "contact";
-            this.contact.Size = new System.Drawing.Size(375, 34);
+            this.contact.Size = new System.Drawing.Size(282, 28);
             this.contact.TabIndex = 65;
             this.contact.Text = "###-####-####";
             this.contact.Click += new System.EventHandler(this.contact_Click);
@@ -209,10 +210,12 @@
             // 
             this.usertype.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usertype.FormattingEnabled = true;
-            this.usertype.Location = new System.Drawing.Point(25, 331);
-            this.usertype.Margin = new System.Windows.Forms.Padding(4);
+            this.usertype.Items.AddRange(new object[] {
+            "Admin",
+            "Employee"});
+            this.usertype.Location = new System.Drawing.Point(19, 269);
             this.usertype.Name = "usertype";
-            this.usertype.Size = new System.Drawing.Size(375, 33);
+            this.usertype.Size = new System.Drawing.Size(282, 28);
             this.usertype.TabIndex = 66;
             this.usertype.Text = "User type";
             // 
@@ -220,10 +223,10 @@
             // 
             this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.email.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.email.Location = new System.Drawing.Point(27, 278);
-            this.email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.email.Location = new System.Drawing.Point(20, 226);
+            this.email.Margin = new System.Windows.Forms.Padding(2);
             this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(375, 34);
+            this.email.Size = new System.Drawing.Size(282, 28);
             this.email.TabIndex = 67;
             this.email.Text = "Email";
             this.email.TextChanged += new System.EventHandler(this.email_TextChanged);
@@ -232,16 +235,18 @@
             // 
             this.gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gender.FormattingEnabled = true;
-            this.gender.Location = new System.Drawing.Point(27, 175);
-            this.gender.Margin = new System.Windows.Forms.Padding(4);
+            this.gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.gender.Location = new System.Drawing.Point(20, 142);
             this.gender.Name = "gender";
-            this.gender.Size = new System.Drawing.Size(375, 33);
+            this.gender.Size = new System.Drawing.Size(282, 28);
             this.gender.TabIndex = 68;
             this.gender.Text = "Gender";
             // 
             // UserControl2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
             this.Controls.Add(this.gender);
@@ -252,15 +257,15 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.search);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Add);
             this.Controls.Add(this.lastname);
             this.Controls.Add(this.firstname);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UserControl2";
-            this.Size = new System.Drawing.Size(1021, 682);
+            this.Size = new System.Drawing.Size(766, 554);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -272,7 +277,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox search;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Add;
         private System.Windows.Forms.TextBox lastname;
         private System.Windows.Forms.TextBox firstname;
         private System.Windows.Forms.Label label2;
