71d84cc3ccb954ebcb34491be55e64bd4d9e8417
fmmmonton9 <34014972+fmmmonton9@users.noreply.github.com>
2018-01-15 18:34:05 +0800

    Update prodcategory.cs

--- SAD2/WindowsFormsApplication4/prodcategory.cs
+++ SAD2/WindowsFormsApplication4/prodcategory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -15,6 +15,11 @@ namespace WindowsFormsApplication4
         public prodcategory()
         {
             InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
