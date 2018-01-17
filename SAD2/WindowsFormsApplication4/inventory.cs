0220a7c184f6b19e94f7c8efb9d3e42ae498fb9e
czshadow09 <reromarj@gmail.com>
2018-01-17 08:08:59 +0800

    Update inventory.cs

--- SAD2/WindowsFormsApplication4/inventory.cs
+++ SAD2/WindowsFormsApplication4/inventory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -7,19 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApplication4
 {
     public partial class inventory : UserControl
     {
+        MySqlConnection conn;
         public inventory()
         {
             InitializeComponent();
+            conn = new MySqlConnection("server=localhost;Database=final;uid=root; Pwd = root;");
+        }
+        private void executeQuery(string q)
+        {
+            conn.Open();
+            MySqlCommand comm = new MySqlCommand(q, conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            loadAll();
         }
 
         private void Out_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void inventory_Load(object sender, EventArgs e)
+        {
+            loadAll();
+        }
+        private void loadAll()
+        {
+            string query = "select * from product;";
+            conn.Open();
+            MySqlCommand com = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(com);
+            conn.Close();
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["product_id"].Visible = false;
+            dataGridView1.Columns["stock_in"].Visible = false;
+            dataGridView1.Columns["stock_out"].Visible = false;
+            dataGridView1.Columns["description"].HeaderText = "Product Name";
+            dataGridView1.Columns["category"].HeaderText = "Category";
+            dataGridView1.Columns["purchase_price"].HeaderText = "Purchase Price";
+            dataGridView1.Columns["store_price"].HeaderText = "Store Price";
+            dataGridView1.Columns["tot_quantity"].HeaderText = "Quantity";
+            dataGridView2.DataSource = dt;
+            dataGridView2.Columns["product_id"].Visible = false;
+            dataGridView2.Columns["description"].HeaderText = "Product Name";
+            dataGridView2.Columns["category"].HeaderText = "Category";
+            dataGridView2.Columns["purchase_price"].HeaderText = "Purchase Price";
+            dataGridView2.Columns["store_price"].HeaderText = "Store Price";
+            dataGridView2.Columns["stock_in"].HeaderText = "In";
+            dataGridView2.Columns["stock_out"].HeaderText = "Out";
+            dataGridView2.Columns["tot_quantity"].HeaderText = "Quantity";
         }
     }
 }
