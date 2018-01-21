2b91a9465172beb66c1c3df20f07f6a8eb8ebf86
czshadow09 <reromarj@gmail.com>
2018-01-21 13:19:40 +0800

    Update In.cs

--- SAD2/WindowsFormsApplication4/In.cs
+++ SAD2/WindowsFormsApplication4/In.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApplication4
 {
     public partial class In : Form
     {
+        MySqlConnection conn;
         public In()
         {
             InitializeComponent();
+            conn = new MySqlConnection("server=localhost;Database=final;uid=root; Pwd = root;");
         }
-
-        private void Add_Click(object sender, EventArgs e)
+        private void executeQuery(string q)
         {
-
+            conn.Open();
+            MySqlCommand comm = new MySqlCommand(q, conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
         }
 
         private void cancel_Click(object sender, EventArgs e)
@@ -27,14 +32,45 @@ namespace WindowsFormsApplication4
             this.Close();
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        private void Add_Click(object sender, EventArgs e)
         {
+            string query = "select stock_in from product where description='" + name.Text + "';";
+            conn.Open();
+            MySqlCommand com = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(com);
+            conn.Close();
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            int input = Int32.Parse(quan.Text);
+            int db = Int32.Parse(dt.Rows[0][0].ToString());
+            int sum = input + db;
+            if (String.IsNullOrEmpty(quan.Text))
+            {
+                MessageBox.Show("Please fill up the field.", "Test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
+            else
+            {
+                string query1 = "UPDATE product SET stock_in='" + sum + "' where description= '" + name.Text + "';";
+                MessageBox.Show("Stocked in '" + quan.Text + "' items!", "Test", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                executeQuery(query1);
+                this.Close();
+            }
         }
 
-        private void quan_TextChanged(object sender, EventArgs e)
+        private void In_Load(object sender, EventArgs e)
         {
-
+            string query = "select description from product;";
+            conn.Open();
+            MySqlCommand com = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(com);
+            conn.Close();
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            for (int x = 0; x < dt.Rows.Count; x++)
+            {
+                name.Items.Add(dt.Rows[x][0].ToString());
+            }
         }
     }
 }
