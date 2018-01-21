bc9abc953255a894e449527a105a9fb80141ddc6
czshadow09 <reromarj@gmail.com>
2018-01-21 13:17:42 +0800

    Update In.Designer.cs

--- SAD2/WindowsFormsApplication4/In.Designer.cs
+++ SAD2/WindowsFormsApplication4/In.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication4
+namespace WindowsFormsApplication4
 {
     partial class In
     {
@@ -29,34 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(In));
-            this.label7 = new System.Windows.Forms.Label();
-            this.quan = new System.Windows.Forms.TextBox();
             this.cancel = new System.Windows.Forms.Button();
             this.Add = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.quan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label7.Location = new System.Drawing.Point(19, 38);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(107, 28);
-            this.label7.TabIndex = 90;
-            this.label7.Text = "Quantity:";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
-            // quan
-            // 
-            this.quan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.quan.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.quan.Location = new System.Drawing.Point(177, 38);
-            this.quan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.quan.Name = "quan";
-            this.quan.Size = new System.Drawing.Size(147, 34);
-            this.quan.TabIndex = 89;
-            this.quan.TextChanged += new System.EventHandler(this.quan_TextChanged);
             // 
             // cancel
             // 
@@ -64,13 +43,13 @@
             this.cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cancel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cancel.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.cancel.Image = global::WindowsFormsApplication4.Properties.Resources.cancel_48;
+            this.cancel.Image = ((System.Drawing.Image)(resources.GetObject("cancel.Image")));
             this.cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cancel.Location = new System.Drawing.Point(187, 101);
-            this.cancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cancel.Location = new System.Drawing.Point(141, 84);
+            this.cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cancel.Name = "cancel";
-            this.cancel.Size = new System.Drawing.Size(170, 66);
-            this.cancel.TabIndex = 92;
+            this.cancel.Size = new System.Drawing.Size(128, 54);
+            this.cancel.TabIndex = 96;
             this.cancel.Text = "Cancel";
             this.cancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.cancel.UseVisualStyleBackColor = true;
@@ -84,29 +63,76 @@
             this.Add.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.Add.Image = ((System.Drawing.Image)(resources.GetObject("Add.Image")));
             this.Add.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Add.Location = new System.Drawing.Point(14, 101);
-            this.Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Add.Location = new System.Drawing.Point(11, 84);
+            this.Add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(178, 66);
-            this.Add.TabIndex = 91;
+            this.Add.Size = new System.Drawing.Size(134, 54);
+            this.Add.TabIndex = 95;
             this.Add.Text = " Stock in";
             this.Add.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.Add.UseVisualStyleBackColor = true;
             this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(11, 49);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 22);
+            this.label7.TabIndex = 94;
+            this.label7.Text = "Quantity:";
+            // 
+            // quan
+            // 
+            this.quan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quan.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.quan.Location = new System.Drawing.Point(130, 49);
+            this.quan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quan.Name = "quan";
+            this.quan.Size = new System.Drawing.Size(111, 28);
+            this.quan.TabIndex = 93;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 98;
+            this.label1.Text = "Name:";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.name.FormattingEnabled = true;
+            this.name.Location = new System.Drawing.Point(130, 9);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(111, 30);
+            this.name.TabIndex = 99;
+            // 
             // In
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
-            this.ClientSize = new System.Drawing.Size(371, 183);
+            this.ClientSize = new System.Drawing.Size(278, 149);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cancel);
             this.Controls.Add(this.Add);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.quan);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "In";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Stock In";
+            this.Load += new System.EventHandler(this.In_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,5 +144,7 @@
         private System.Windows.Forms.Button Add;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox quan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox name;
     }
 }
